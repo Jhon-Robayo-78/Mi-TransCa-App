58d045592c348e03ca734711446ae973ec9aea28
vianrove <thuanromero@gmail.com>
2023-05-24 00:12:18 -0500

    routes data imported from src

--- src/routes/routesT.jsx
+++ src/routes/routesT.jsx
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import routes from "./routesData.json"
 import RouteComponent from '../components/RouteComponent';
 
 const RoutesT = ()=>{
-    const [routes, setRoutes] = useState([]);
-
-        useEffect(() => {
-            // Fetch the routes data from routesData.json
-            fetch("src/routesData.json")
-            .then((response) => response.json())
-            .then((data) => setRoutes(data.routes))
-            .catch((error) => console.log(error));
-        }, []);
-
+    
         return (
             <div>
-              <RouteComponent routes={routes} />
+              <RouteComponent routes={routes["routes"]} />
             </div>
           );
 }
